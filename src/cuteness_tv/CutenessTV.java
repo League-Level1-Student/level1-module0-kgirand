7ae94e7a8a5d7616771aaa6bb12569dee52c9f08
kgirand <44352919+kgirand@users.noreply.github.com>
2019-05-14 19:26:58 -0700

    Add files via upload

--- src/cuteness_tv/CutenessTV.java
+++ src/cuteness_tv/CutenessTV.java
@@ -1,25 +1,61 @@
 package cuteness_tv;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class CutenessTV implements ActionListener {
+JButton button1 = new JButton();
+JButton button2 = new JButton();
+JButton button3 = new JButton();
 	public void showButton() {
 		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
 		frame.setVisible(true);
-		JButton button1 = new JButton();
-		frame.add(button1);
+		panel.add(button1);
 		frame.pack();
 		button1.addActionListener(this);
-		JButton button2 = new JButton();
-		frame.add(button2);
+		panel.add(button2);
 		frame.pack();
 		button2.addActionListener(this);
-		JButton button3 = new JButton();
-		frame.add(button3);
+		panel.add(button3);
 		frame.pack();
 		button3.addActionListener(this);
 	}
-	
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+	void playVideo(String videoID) {
+	     try {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	if(e.getSource()==button1) {
+		showDucks();
+	}
+	if(e.getSource()==button2) {
+		showFrog();
+	}
+	if(e.getSource()==button3) {
+		showFluffyUnicorns();
+	}
+	}
 }
---
+++ src/cuteness_tv/cuteness_runner.java
@@ -0,0 +1,8 @@
+package cuteness_tv;
+
+public class cuteness_runner {
+public static void main(String[] args) {
+	CutenessTV c = new CutenessTV();
+	c.showButton();
+}
+}
